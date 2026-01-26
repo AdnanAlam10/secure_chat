c97b0844ac34a45747834643f757e0bb4caea3dc
Adnan Alam <adnanalamcie@gmail.com>
2026-01-26 13:54:44 +1100

    feat: elysiajs provider and route setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
+import { useMutation } from "@tanstack/react-query";
+import { client } from "@/lib/client";
 
 const ANIMALS = ["wolf", "hawk", "bear", "shark"];
 const STORAGE_KEY = "chat_username";
@@ -30,6 +32,12 @@ export default function Home() {
     main();
   }, []);
 
+  const { mutate: createRoom } = useMutation({
+    mutationFn: async () => {
+      const res = await client.room.create.post();
+    },
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
       <div className="w-full max-w-md space-y-8">
@@ -54,7 +62,10 @@ export default function Home() {
               </div>
             </div>
 
-            <button className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50">
+            <button
+              onClick={() => createRoom()}
+              className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50"
+            >
               CREATE SECURE ROOM
             </button>
           </div>
